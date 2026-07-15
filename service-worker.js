f06a945fde0634be4f7de1ecff8999ed127663ee
Apkils <apifnofal8@gmail.com>
2026-07-15 19:31:15 +0700

    feat: setup PWA, manifest, dan service worker selesai

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'rantauflow-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/manifest.json',
+  '/public/icon-512.png'
+];
+
+// Saat aplikasi pertama kali diinstal
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Menangani request jaringan
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Kembalikan dari cache jika ada, jika tidak, ambil dari internet
+        return response || fetch(event.request);
+      })
+  );
+});
